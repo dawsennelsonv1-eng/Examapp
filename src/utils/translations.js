e4501f0cef04d3ce7d270c72b6bbbe81b6f6dce6
dawsennelsonv1-eng <dawsennelsonv1@gmail.com>
2026-05-25 08:24:19 -0400

    Add files via upload

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -1,5 +1,6 @@
 // src/utils/translations.js
-// Central dictionary. Every UI string flows through useTranslation(key).
+// French-only translations for the UI.
+// All keys used anywhere in the app are defined here to avoid undefined fallbacks.
 
 export const translations = {
   fr: {
@@ -9,110 +10,96 @@ export const translations = {
     track_9af: "9ème AF",
     track_ns4: "Nouveau Secondaire IV",
 
-    // Tabs
-    tab_home: "Accueil",
-    tab_scan: "Scanner",
-    tab_quiz: "Quiz",
-    tab_vault: "Archives",
-    tab_social: "Sc. Sociales",
-
     // Home
-    countdown_prefix: "Tu es à l'examen du 17 juillet.",
-    countdown_question: "Que répondrais-tu ?",
-    countdown_asked: "Voici ce qu'ils ont demandé en",
-    daily_mission: "Mission du jour",
-    master_formulas: "Maîtrise ces formules aujourd'hui",
-    leaderboard: "Classement anonyme",
+    daily_mission: "Missions du jour",
+    leaderboard: "Classement",
+    countdown_prefix: "Le compte à rebours est lancé.",
+    countdown_question: "Tu es prêt à donner le meilleur de toi ?",
+    master_formulas: "Maîtrise 2 formules",
 
-    // Scan & Solve
-    scan_title: "Scanner un problème",
-    scan_placeholder: "Pointe la caméra sur ton exercice",
-    scan_capture: "Capturer",
-    scan_upload: "Importer une photo",
-    solution_heading: "Solution pas à pas",
-    unblur_next: "Débloquer l'étape suivante",
-    see_all: "Voir toute la solution",
-    explain_differently: "Explique-moi autrement",
-    hypothese: "Hypothèse",
-    formule: "Formule",
-    resolution: "Résolution",
-    audio_listen: "Écouter l'explication",
-    audio_stop: "Arrêter",
+    // Scan
+    scan_capture: "Cadrer l'exercice",
+    scan_solving: "Résolution en cours...",
+    scan_understand_question: "Je comprends pas, explique-moi",
+    scan_new_problem: "Nouveau",
+
+    // Classroom
+    classroom_title: "Salle de classe",
+    classroom_subtitle: "Pose tes questions au prof",
+    classroom_empty_title: "Ta salle de classe est vide",
+    classroom_empty_subtitle: "Scanne un exercice ou démarre une conversation",
+    classroom_understand: "Oui, je comprends",
+    classroom_not_understand: "Non, explique encore",
+    classroom_listen: "Écouter",
+    classroom_stop: "Arrêter",
 
     // Quiz
-    quiz_submit: "Valider",
-    quiz_next: "Suivant",
-    tested_in: "Cette question a été testée à l'examen de",
-    view_original: "Voir l'original",
-    cheat_sheet: "Révision de dernière minute",
+    quiz_title: "Quiz d'examen",
+    quiz_choose_subject: "Choisis une matière",
+    quiz_no_questions: "Pas encore généré",
+    quiz_validate: "Valider",
+    quiz_next: "Question suivante",
+    quiz_see_results: "Voir les résultats",
+    quiz_redo: "Refaire le quiz",
+    quiz_other_subject: "Choisir une autre matière",
+    quiz_excellent: "Excellent !",
+    quiz_not_bad: "Pas mal",
+    quiz_keep_working: "Continue à travailler",
+    quiz_explanation: "Explication",
+    quiz_trap: "Piège typique",
 
-    // Vault
-    past_exams: "Anciens examens",
-    trap_detector: "Détecteur de pièges",
-    mind_body: "Corps & Esprit",
+    // Profile
+    profile_personality: "Personnalité du prof",
+    profile_language: "Langue de discussion",
+    profile_plan: "Plan d'abonnement",
+    profile_settings: "Paramètres",
+    profile_theme_dark: "Thème sombre",
+    profile_theme_light: "Thème clair",
+    profile_toggle_theme: "Toucher pour basculer",
+    profile_reset: "Réinitialiser tout",
+    profile_reset_subtitle: "Recommencer depuis zéro",
+    profile_upgrade: "Passer en Premium",
+    profile_upgrade_subtitle: "Scans, chats, voix HD illimités",
 
-    // Common
+    // Reviser
+    reviser_title: "Réviser",
+    reviser_subtitle: "Choisis une matière, le prof t'aide à réviser",
+    reviser_start: "Commencer",
+    reviser_tip_title: "💡 Astuce",
+    reviser_tip_body:
+      "Pose des questions précises comme \"Explique-moi le théorème de Pythagore\" ou \"Fais-moi un schéma du système solaire\".",
+
+    // Paywall
+    paywall_title: "Débloque tout le potentiel",
+    paywall_subtitle: "Le MENFP arrive vite. Prépare-toi sans limites.",
+    paywall_plan_basic: "Basic",
+    paywall_plan_premium: "Premium",
+    paywall_per_month: "HTG / mois",
+    paywall_pay_title: "Paiement",
+    paywall_send_to: "Envoie",
+    paywall_to_one_number: "à l'un de ces numéros",
+    paywall_copy: "Copier",
+    paywall_tx_id_label: "ID de transaction",
+    paywall_tx_id_placeholder: "Ex: 12345678 ou ABC123XYZ",
+    paywall_activate: "Activer mon plan",
+    paywall_important: "Important",
+    paywall_important_body:
+      "Après avoir payé, tu recevras un SMS avec un ID de transaction. Colle cet ID ci-dessous pour activer ton plan.",
+
+    // Errors / common
+    error_no_internet: "Pas de connexion internet. Réessaye.",
+    error_unclear_image: "L'image n'est pas assez claire. Reprends la photo.",
+    error_server: "Erreur. Réessaye dans un moment.",
+    button_retry: "Réessayer",
+    button_back: "Retour",
+    button_continue: "Continuer",
+    button_cancel: "Annuler",
+    button_save: "Enregistrer",
+    button_delete: "Supprimer",
     loading: "Chargement...",
-    error_generic: "Une erreur est survenue. Réessaie.",
-    offline_mode: "Mode hors ligne",
-  },
-  ht: {
-    // Onboarding
-    onboarding_title: "Ki egzamen w ap kraze ?",
-    onboarding_subtitle: "Chwazi wout ou. Nou okipe rès la.",
-    track_9af: "9èm AF",
-    track_ns4: "Nouvo Segondè IV",
 
-    // Tabs
-    tab_home: "Akèy",
-    tab_scan: "Skane",
-    tab_quiz: "Kwiz",
-    tab_vault: "Achiv",
-    tab_social: "Sy. Sosyal",
-
-    // Home
-    countdown_prefix: "Ou nan egzamen 17 jiyè a.",
-    countdown_question: "Kisa w t ap reponn ?",
-    countdown_asked: "Men sa yo te mande nan",
-    daily_mission: "Misyon jou a",
-    master_formulas: "Metrize fòmil sa yo jodi a",
-    leaderboard: "Klasman anonim",
-
-    // Scan & Solve
-    scan_title: "Skane yon pwoblèm",
-    scan_placeholder: "Pwente kamera a sou egzèsis ou",
-    scan_capture: "Pran foto",
-    scan_upload: "Chaje yon foto",
-    solution_heading: "Solisyon etap pa etap",
-    unblur_next: "Dekouvri pwochèn etap",
-    see_all: "Wè tout solisyon an",
-    explain_differently: "Eksplike m sa yon lòt jan",
-    hypothese: "Ipotèz",
-    formule: "Fòmil",
-    resolution: "Rezolisyon",
-    audio_listen: "Eksplike m sa",
-    audio_stop: "Rete",
-
-    // Quiz
-    quiz_submit: "Valide",
-    quiz_next: "Pwochen",
-    tested_in: "Kesyon sa a te parèt nan egzamen",
-    view_original: "Wè orijinal la",
-    cheat_sheet: "Revizyon dènye minit",
-
-    // Vault
-    past_exams: "Ansyen egzamen",
-    trap_detector: "Detektè pyèj",
-    mind_body: "Kò & Lespri",
-
-    // Common
-    loading: "K ap chaje...",
-    error_generic: "Gen yon pwoblèm. Eseye ankò.",
-    offline_mode: "San entènèt",
+    // Tutor greetings (used as fallbacks)
+    tutor_greeting_with_name: "Bonjou {name} ! Ki sa ou vle nou travay ansanm jodi a ?",
+    tutor_greeting: "Bonjou ! M se pwofesè ou a. Ki pwoblèm ou vle nou travay ?",
   },
 };
-
-export const LANGUAGES = [
-  { code: "fr", label: "Français" },
-  { code: "ht", label: "Kreyòl" },
-];
